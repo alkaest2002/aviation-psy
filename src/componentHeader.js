b8d54f480208369dcf16e0e11625ca2bc2d8752e
Pierpaolo Calanna <p.calanna@gmail.com>
2026-04-19 20:45:20 +0200

    Update site

---
+++ src/componentHeader.js
@@ -0,0 +1,11 @@
+export const header = () => ({
+    
+    homeLink: {
+        ["@click.prevent"]() {
+            const a = Object.assign(document.createElement('a'), { href: '/' });
+            document.body.append(a);
+            a.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true }));
+            a.remove();
+        }
+    }
+})
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Alpine from "alpinejs"
 import programJSON from "./program.json"
-import { spa } from "./spa.js";
-import { program } from "./program.js";
-import { switcher } from "./switcher.js";
-import { header } from "./header.js";
+import { spa } from "./componentSpa.js";
+import { program } from "./componentProgram.js";
+import { switcher } from "./componentSwitcher.js";
+import { header } from "./componentHeader.js";
 
 window.Alpine = Alpine
 
@@ -14,8 +14,8 @@ document.addEventListener("alpine:initialized", () => {
 Alpine.store("programStore", programJSON)
 
 Alpine.data("spa", spa)
-Alpine.data("program", program)
-Alpine.data("switcher", switcher)
 Alpine.data("header", header)
+Alpine.data("switcher", switcher)
+Alpine.data("program", program)
 
 Alpine.start()
