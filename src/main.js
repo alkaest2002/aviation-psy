04fb2b17e3c0e3ebfafd6231db672ae69fcdb8da
Pierpaolo Calanna <p.calanna@gmail.com>
2026-04-19 20:57:04 +0200

    Simplified js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import Alpine from "alpinejs"
 import programJSON from "./program.json"
 import { spa } from "./componentSpa.js";
-import { program } from "./componentProgram.js";
 import { header } from "./componentHeader.js";
 
 window.Alpine = Alpine
-
-document.addEventListener("alpine:initialized", () => {
-  console.log("Alpine initialized.");
-});
 
 Alpine.store("programStore", programJSON)
 
 Alpine.data("spa", spa)
 Alpine.data("header", header)
-Alpine.data("program", program)
 
 Alpine.start()
